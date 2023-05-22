4fe909f2342171a223d27bc6a9b26b1be4cab439
Kaiser338 <46496227+Kaiser338@users.noreply.github.com>
2023-05-22 19:46:51 +0200

    backend update

--- server/core/views.py
+++ server/core/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -7,6 +8,7 @@ from rest_framework import generics
 from django.shortcuts import get_object_or_404
 from .models import Game, Platform, Genre, Publisher, Developer
 from .serializers import GameSerializer, PlatformSerializer, GenreSerializer, PublisherSerializer, DeveloperSerializer
+from django.db.models import Q
 
 # Create your views here.
 
@@ -29,6 +31,7 @@ class LoginView(APIView):
         return Response(serializer.errors, status=400)
 
 
+
 class GameView(viewsets.ViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
@@ -38,9 +41,48 @@ class GameView(viewsets.ViewSet):
         serializer = self.serializer_class(instance)
         return Response(serializer.data)
 
-    def list(self, request, *args, **kwargs):
-        serializer = self.serializer_class(self.queryset, many=True)
+    def list(self, request):
+        search_query = request.query_params.get('search', '')
+        queryset = self.queryset.filter(
+            Q(gameName__icontains=search_query) | Q(description__icontains=search_query)
+        )
+        serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
+    
+
+    def get_top_rated_games(self, request):
+        num_games = request.query_params.get('num_games', 5) 
+
+        try:
+            num_games = int(num_games)
+            if num_games <= 0:
+                raise ValueError("Invalid value for 'num_games'.")
+        except ValueError:
+            return Response({'error': "Invalid value for 'num_games'. Must be a positive integer."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        queryset = self.queryset.order_by('-rating')[:num_games]
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_games_by_genre(self, request, genre_name=None):
+        num_games = request.GET.get('num_games')
+        try:
+            if num_games:
+                num_games = int(num_games)
+                if num_games <= 0:
+                    raise ValueError("Invalid value for 'num_games'.")
+        except ValueError:
+            return Response({'error': "Invalid value for 'num_games'. Must be a positive integer."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        queryset = Game.objects.filter(genres__name__icontains=genre_name)
+        if num_games:
+            queryset = queryset[:num_games]
+        
+        serializer = GameSerializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class PlatformView(viewsets.ModelViewSet):
     queryset = Platform.objects.all()
