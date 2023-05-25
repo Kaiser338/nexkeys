25f8c5efc66abafba006daadb625cc4b1dc1c262
Kaiser338 <46496227+Kaiser338@users.noreply.github.com>
2023-05-25 12:08:52 +0200

    client update

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,7 +1,23 @@
+import Product from '@components/product';
+import '@styles/globals.css';
+import '@styles/home_page.css';
+
 const Home = () => {
   return (
-    <section className="w-full flex-center">
-
+    <section className='main-page-section'>
+      <div className='title-bar'>
+        <span className='title-bar-text'>
+          Bestsellers
+        </span>
+      </div>
+      <div className='product-section'>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+        <Product/>
+      </div>
     </section>
   )
 
---
+++ client/components/product.jsx
@@ -0,0 +1,16 @@
+import '@styles/product.css';
+
+const Product = () => {
+    return (
+        <div>
+            <div class="image-component">
+                <img src="https://image.api.playstation.com/vulcan/ap/rnd/202301/0312/GU1vXFJpbzGYNV6UN3U0Cnnb.png" alt="Image"/>
+                <div class="component-title">Fifa 23 sadasd asd asd asd </div>
+                <div class="component-price">$9.99</div>
+            </div>
+        </div>
+    )
+  
+  }
+  
+  export default Product
